f667ec957ed510929a014c83d744e6853ad7ace8
Hein <rhovanion115@gmail.com>
2026-05-25 10:30:31 +0630

    Inngest Function for clerk user create, updated and deleted

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import {
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/inngest/functions";
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+});
